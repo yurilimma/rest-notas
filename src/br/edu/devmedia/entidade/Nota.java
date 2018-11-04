fe9d2350666569af80b14434728f35054432c504
Yuri Lima <yurilimmarabelo@gmail.com>
2018-11-04 19:11:59 -0200

    restful java

---
+++ src/br/edu/devmedia/entidade/Nota.java
@@ -0,0 +1,29 @@
+package br.edu.devmedia.entidade;
+
+public class Nota {
+	
+	private Integer id;
+	private String titulo;
+	private String descricao;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+
+}
